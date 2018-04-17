429453400828ca6122c77bd06c186fe94adaf0b7
Robert Spork <rspork@gv029d491.gv029.kpn-post.nl>
2018-04-17 14:13:35 +0200

    docker-compose

--- startup.sh
+++ startup.sh
@@ -1,2 +1,8 @@
+curl -L https://github.com/docker/compose/releases/download/1.3.3/docker-compose-`uname -s`-`uname -m` > docker-compose
+mkdir /opt/
+mkdir /opt/bin
+mv docker-compose /opt/bin/docker-compose
+
+
 docker run --rm --name docker-nginx -p 80:80 -d nginx
 docker run --rm --name docker-apache -p 81:80 -d httpd
